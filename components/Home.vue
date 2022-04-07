0457ce3df71a7322f47aeae5f41ff77a6b05d6f7
linkai <1134842727@qq.com>
2022-04-07 10:25:00 +0800

    init project

---
+++ components/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="el-header">Header</el-header>
+      <el-container class="el-container">
+        <el-aside class="el-aside">
+
+  <el-row class="tac">
+    <el-col>
+      <h5 class="mb-2">Custom colors</h5>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="#fff"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item one</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<style>
+*{
+    padding: 0;
+    margin: 0;
+}
+.el-header {
+    background-color:#262f3e;
+    color: white;
+    line-height: 55px;
+}
+.el-aside {
+    width: 150px;
+    height: 500px;
+    color: white;
+    background-color: #1e222d;
+}
+
+
+</style>
